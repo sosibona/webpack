7c80a8b1941806ad7ccc9c742bb97c09f9001d33
sosibona <empty.tara@gmail.com>
2020-03-20 23:02:35 +0200

    commit

---
+++ lesson1/task2/src/scripts/deleteTask.js
@@ -0,0 +1,27 @@
+import { renderListItems, listElem } from './index.js';
+import { deleteTask, getTasksList }  from './tasksGateway.js'
+import {setItem, getItem} from './storage.js'
+
+const deleteBtn = document.querySelector('.list');
+
+
+export const onDeleteTask = event => {
+  // const listTaskNow = event.target.closest('.list__item-checkbox');
+  const thisTask = event.target.classList.contains('delete-btn');
+  
+  if (!thisTask) {
+    return;
+  }
+
+  const taskId = event.target.parentNode.dataset.id;
+  
+  deleteTask(taskId)
+    .then(() => getTasksList())
+    .then(tasks => {
+      setItem('tasksList', tasks); 
+      listElem.innerHTML = '';
+      renderListItems(); 
+    });
+}
+
+deleteBtn.addEventListener('click', onDeleteTask);
