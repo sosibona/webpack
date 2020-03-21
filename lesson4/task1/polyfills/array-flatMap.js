98656c2a39cd5af802439579ab528d7ac613d3f9
sosibona <empty.tara@gmail.com>
2020-03-21 19:34:09 +0200

    sol

---
+++ lesson4/task1/polyfills/array-flatMap.js
@@ -0,0 +1,7 @@
+// здесь никакие экспорты не нужны
+
+console.log('polyfill for flatMap');
+
+Array.prototype.flatMap = Array.prototype.flatMap || function flatMap() {
+    // ... implementation for older browsers
+};
