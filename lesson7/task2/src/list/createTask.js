0ffa1d1c35640c35205cd42ebf9a8743756a6ac0
sosibona <empty.tara@gmail.com>
2020-03-24 13:32:50 +0200

    sol2

---
+++ lesson7/task2/src/list/createTask.js
@@ -0,0 +1,33 @@
+import {setItem, getItem} from './storage.js'
+import { renderListItems, listElem } from '../index.js';
+import { createTask, getTasksList } from './tasksGateway.js';
+
+const btnCreateTask = document.querySelector('.create-task-btn');
+let getTextFromInput = document.querySelector('.task-input')
+
+btnCreateTask.addEventListener('click', createNewTask);
+
+export function createNewTask(){
+    let inputValue = getTextFromInput.value;
+    if (inputValue.length === 0) return;
+
+    getTextFromInput.value = '';
+
+    const newTask = {
+      text: inputValue,
+      dateCreate: new Date(),
+      done: false,
+      dateConfirmed: null, 
+      id: Math.random().toString()   
+    }
+
+    createTask(newTask)
+      .then(() => getTasksList())
+      .then(tasks => {
+        setItem('tasksList', tasks); 
+        listElem.innerHTML = '';
+        renderListItems(); 
+      });
+
+          
+}
